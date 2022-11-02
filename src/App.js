27e2a60a64e29fd07917d7a2088a66bd0d39bc5f
Chidera-Ok <okekechidera98@gmail.com>
2022-11-02 02:55:09 -0700

    done with implementation of web and mobile design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile_pic from "./images/my_image.jpg";
+import doc_vector from "./images/dot-Vector.svg"
+import icon_git from "./images/Icon-git.svg"
+import i4G from "./images/I4G.svg"
+import icon_slack from "./images/icon-slack.svg"
+import zuri from "./images/zuri.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+        <div className="container">
+          <main className="inner-container">
+            <img src={profile_pic} alt="profile image" id="profile_img" />
+            <h2 id="twitter">ChidezOk</h2>
+            <h2 id="slack" hidden>ChideraOk</h2>
+            <a href="https://twitter.com/ChidezOk/" id="twitter_link">
+              <p> Twitter Link </p>
+            </a>
+            <a href="https://training.zuri.team/" id="btn_zuri">
+              <p> Zuri Team </p>
+            </a>
+            <a href="http://books.zuri.team/" id="books">
+              <p> Zuri Books </p>
+            </a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=ChideraOk" id="book_python">
+              <p> Python Books </p>
+            </a>
+            <a href="https://background.zuri.team/" id="pitch">
+              <p> Background Check For Coders </p>
+            </a>
+            <a href="https://books.zuri.team/design-rules" id="book__design">
+              <p>
+                Design Books
+              </p>
+            </a>
+            <div className="social">
+              <img src={icon_slack} alt="slack icon" />
+              <img src={icon_git} alt="github icon" />
+            </div>
+          </main>
+        </div>
+        <footer>
+          <div className="zuri-logo">
+            <img src={zuri} alt="zuri icon" id="writeup" />
+            <img src={doc_vector} alt="dot vector" id="dot" />
+          </div>
+          <p id="hng">HNG Internship 9 Frontend Task</p>
+          <img src={i4G} alt="hng icon" />
+        </footer>
+      </div>
+    );
 }
 
 export default App;
